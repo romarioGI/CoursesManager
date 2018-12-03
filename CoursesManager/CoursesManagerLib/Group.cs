032f575f36cbc9106c1a78720aab68372db32cda
romarioGI <roma.fenc@yandex.ru>
2018-12-04 01:01:12 +0300

    WPF refactoring

--- CoursesManager/CoursesManagerLib/Group.cs
+++ CoursesManager/CoursesManagerLib/Group.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CoursesManagerLib
 {
@@ -8,66 +9,67 @@ namespace CoursesManagerLib
     public class Group:IEnumerable<Client>
     {
         private static int _lastId = 0;
+        private static ClientNameComparator _clientCmp = new ClientNameComparator();
+        private readonly Attendance _attendance;
 
         public readonly int Id;
         public readonly Course Course;
-        public readonly Attendance Attendance;
 
-        private readonly List<Client> _сlients;
+        private readonly List<Client> _clients;
 
         public int CountClients
         {
-            get { return _сlients.Count; }
+            get { return _clients.Count; }
         }
 
         public Client this[int index]
         {
-            get { return _сlients[index]; }
+            get { return _clients[index]; }
         }
 
         public Group(Course course)
         {
             Id = _lastId++;
             Course = course;
-            _сlients = new List<Client>();
-            Attendance = new Attendance();
+            _clients = new List<Client>();
+            _attendance = new Attendance();
         }
 
         public bool CanAddClient()
         {
-            return (_сlients.Count < 10);
+            return (_clients.Count < 10);
         }
 
         public void AddClient(Client client) //добавление клиента в группу
         {
-            if (_сlients.Count > 9)
+            if (_clients.Count > 9)
                 throw new ArgumentException("This group is full.");
 
-            _сlients.Add(client);
+            _clients.Add(client);
             client.JoinGroup(this);
-
+            _clients.Sort(_clientCmp);
         }
 
         public int GetCount()
         {
-            return _сlients.Count;
+            return _clients.Count;
         }
 
-        public void RemoveClient(Client client) //удаление клиента в группу
+        public void RemoveClient(Client client) //удаление клиента из группы
         {
-            if (_сlients.Count < 6)
+            if (_clients.Count < 6)
                 throw new ArgumentException("This group is too small.");
 
-            if (_сlients.Contains(client) == false)
+            if (_clients.Contains(client) == false)
                 throw new ArgumentException("This client is not a member of this group.");
 
-            _сlients.Remove(client); //сработает ли? может надо дописать сравнение для данного типа?
+            _clients.Remove(client); 
             client.LeaveGroup(this);
         }
 
         public IEnumerator<Client> GetEnumerator()
         {
-            foreach (var c in _сlients)
+            foreach (var c in _clients)
                 yield return c;
         }
 
@@ -80,7 +82,7 @@ namespace CoursesManagerLib
         {
             var s = "Course\n";
             s += Course.ToString()+"\n";
-            s += "Students    " + _сlients.Count.ToString() + "\n";
+            s += "Students    " + _clients.Count.ToString() + "\n";
             var i = 0;
             foreach (var cl in this)
             {
@@ -90,14 +92,42 @@ namespace CoursesManagerLib
             return s;
         }
 
-        public SortedList<Client, SortedList<DateTime, bool>> GetAttendance()
+        public SortedList<Client, SortedList<DateTime, bool?>> GetAttendance()
         {
-            return Attendance.GetAttendance(_сlients);
+            var res = new SortedList<Client, SortedList<DateTime, bool?>>(_clientCmp);
+            var att = _attendance.GetAttendance(_clients);
+
+            var dates = new SortedSet<DateTime>();
+            foreach(var dateList in att.Values)
+                foreach(var d in dateList.Keys)
+                    dates.Add(d);
+
+            foreach (var client in att.Keys)
+            {
+                res.Add(client,new SortedList<DateTime, bool?>());
+                foreach (var date in dates)
+                {
+                    if (att[client].ContainsKey(date))
+                        res[client][date] = att[client][date];
+                    else
+                        res[client][date] = null;
+                }
+            }
+
+            return res;
+        }
+
+        public void SetAttendance(List<Client> attendedClients, DateTime date)
+        {
+            _attendance.MarkAttendance(_clients, attendedClients, date);
+
+            foreach (var c in attendedClients)
+                c.ChangeAccountSum(-1 * Course.Price);
         }
 
         public bool FindClient(Client client)
         {
-            return _сlients.Contains(client);
+            return _clients.Contains(client);
         }
     }
 }
---
+++ CoursesManager/CoursesManagerWPF/SetAttendanceWindow.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using CoursesManagerLib;
+
+namespace CoursesManagerWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для SetAttendanceWindow.xaml
+    /// </summary>
+    public partial class SetAttendanceWindow : Window
+    {
+        private School _school;
+        private CheckBox[] _checkBoxs;
+
+        //!!!!
+        public SetAttendanceWindow(School school, SchoolUpdate schoolUpdateEvent)
+        {
+            InitializeComponent();
+            _school = school;
+
+
+            /*var g = new Group(new Course(" ", 1, " ", Format.Group, 3, 3));
+
+            _school = new School();
+
+            _school.Groups.Add(g);
+            g.AddClient(new Client("TEST","teat"));
+            g.AddClient(new Client("Romka","Pomka"));*/
+
+            schoolUpdateEvent += UpdateGroupIdComboBox;
+
+            UpdateGroupIdComboBox();
+            InitGrid();
+        }
+
+        private void UpdateGroupIdComboBox()
+        {
+            GroupIdComboBox.SelectedIndex = -1;
+            GroupIdComboBox.ItemsSource = _school.Groups.Select(x => x.Id).ToArray();
+        }
+
+        private void InitGrid()
+        {
+            _checkBoxs = new CheckBox[10];
+            for (var i = 0; i < 10; i++)
+            {
+                _checkBoxs[i] = new CheckBox();
+                var rb = _checkBoxs[i];
+                rb.HorizontalAlignment = HorizontalAlignment.Center;
+                rb.VerticalAlignment = VerticalAlignment.Center;
+                rb.Content = "YES";
+                rb.FontSize = 20;
+                ChangeGridField(AttendanceGrid, i, 1, rb);
+                rb.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void ReturnButton_Click(object sender, RoutedEventArgs e)
+        {
+            Visibility = Visibility.Hidden;
+            Owner.Visibility = Visibility.Visible;
+            GroupIdComboBox.SelectedIndex = -1;
+        }
+
+        private void GroupIdComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            AttendanceGrid.Children.Clear();
+            InitGrid();
+
+            if (GroupIdComboBox.SelectedIndex == -1)
+                return;
+
+            var group = _school.Groups[GroupIdComboBox.SelectedIndex];
+            var cntClients = group.GetCount();
+
+            for (var i = 0; i < cntClients; i++)
+            {
+                var l = new Label
+                {
+                    Content = group[i].Surname + " " + group[i].Name + "\nId: " + group[i].Id, FontSize = 20,
+                    HorizontalAlignment = HorizontalAlignment.Left, VerticalAlignment = VerticalAlignment.Center
+                };
+                ChangeGridField(AttendanceGrid, i, 0, l);
+
+                _checkBoxs[i].Visibility = Visibility.Visible;
+            }
+        }
+
+        private void ChangeGridField(Grid grid, int i, int j, UIElement content)
+        {           
+            Grid.SetColumn(content, j);
+            Grid.SetRow(content, i);
+            grid.Children.Add(content);            
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (GroupIdComboBox.SelectedIndex == -1)
+            {
+                MessageBox.Show("No group selected. Select a group and save again.");
+                return;
+            }
+
+            if (Date.SelectedDate == null)
+            {
+                MessageBox.Show("No date selected. Select a date and save again.");
+                return;
+            }
+
+            var gr = _school.Groups[GroupIdComboBox.SelectedIndex];
+            var date = Date.SelectedDate.Value;
+            var attCl = new List<Client>();
+
+            for(var i=0;i<gr.CountClients;i++)
+                if(_checkBoxs[i].IsChecked == true)
+                    attCl.Add(gr[i]);
+
+            try
+            {
+                gr.SetAttendance(attCl, date);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(
+                    "Something went wrong. Most likely, the lesson with this date already been carried out.");
+                return;
+            }
+            MessageBox.Show("Successfully!");
+            GroupIdComboBox.SelectedIndex = -1;
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if(this.Visibility == Visibility.Visible)
+                this.Owner.Close();
+        }
+    }
+}
